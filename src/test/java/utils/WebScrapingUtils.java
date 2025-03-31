80abac3003d1ef33c121a7b7cc9563ea9679dd07
Shahan <k.shahan786@gmail.com>
2025-03-31 09:38:59 +0100

    added expected launch date too

--- src/test/java/utils/WebScrapingUtils.java
+++ src/test/java/utils/WebScrapingUtils.java
@@ -35,21 +35,21 @@ public class WebScrapingUtils {
     public static void webScraper() {
         List<WebElement> bikeNamesElement = driver.findElements(By.cssSelector("a[data-track-label='model-name'] strong"));
         List<WebElement> bikePriceElement = driver.findElements(By.cssSelector("div.b.fnt-15"));
-        Base.logger.info("Pulling Bike model names from site");
+        List<WebElement> bikeLaunchDateElement = driver.findElements(By.cssSelector(".clr-try.fnt-14")); // Fetching launch dates
+        Base.logger.info("Pulling Bike model names, prices, and expected launch dates from site");
 
         for (int i = 0; i < bikeNamesElement.size(); i++) {
             String bikeName = bikeNamesElement.get(i).getText();
             String bikePrice = bikePriceElement.get(i).getText();
+            String launchDate = bikeLaunchDateElement.get(i).getText(); // Get the launch date
             String convertedPrice = convertPrice(bikePrice);
 
             if (convertedPrice.equalsIgnoreCase("Price to be announced")) {
                 // Display bikes with no price
-                System.out.println("Bike Name: " + bikeName + "     Bike Price: Price to be announced");
-            } else {
+                System.out.println("Bike Name: " + bikeName + "     Bike Price: Price to be announced     Expected Launch: " + launchDate);            } else {
                 int numericPrice = Integer.parseInt(convertedPrice); // Proper parsing of numeric price
                 if (numericPrice < 400000) { // Only display bikes with price below 4 lakh
-                    System.out.println("Bike Name: " + bikeName + "     Bike Price: Rs. " + convertedPrice);
-                }
+                    System.out.println("Bike Name: " + bikeName + "     Bike Price: Rs. " + convertedPrice + "    " + launchDate);                }
             }
         }
     }
