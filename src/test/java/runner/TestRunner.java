6856bca565fafdade90a6367e175136211a58d86
Shahan <k.shahan786@gmail.com>
2025-04-08 10:49:01 +0100

    fixed cucumber report filepath

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,6 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features/",
         glue = {"stepdefinitions", "hooks"},
-        tags = "@FilteredBikes",//Specifies the package containing the step definitions.
         plugin = {"pretty", "html:src/test/resources/CucumberReports/cucumber-reports","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
         //this allows generation of reports
 )
